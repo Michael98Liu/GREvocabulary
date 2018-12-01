067d573022110c148a0e4fc8a81ae9011c87edc5
SamSamhuns <sthapuskar@yahoo.com>
2018-11-30 21:43:20 -0500

    Added Added url patterns in GREvocabulary urls.py

--- GREvocabulary/urls.py
+++ GREvocabulary/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
 
 urlpatterns = [
+    path('GRE/', include('GRE.urls')),
     path('admin/', admin.site.urls),
 ]
