eedb1969483069382859c75b900a507e17fd3548
chennyhaohao <chennyhaohao@gmail.com>
2018-12-01 02:51:55 +0000

    create models

--- GRE/models.py
+++ GRE/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length = 80)
+
+class Word(models.Model):
+    word = models.CharField(max_length = 50)
+    meaning = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+            null=True)
+    
+
 # Create your models here.
