5d461193ae32abb2e0804467159dfbff1e4ed8c5
Michael98Liu <liufengyuan1998@hotmail.com>
2018-12-06 05:05:48 +0000

    add server IP

--- GREvocabulary/settings.py
+++ GREvocabulary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-xrmon$r@m%tggf9pyoxwsq)+-_2=%2*o1e8ths5dpnx#=0mn7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.196.62.105', '127.0.0.1', '104.196.180.37']
+ALLOWED_HOSTS = ['35.227.42.0', '35.196.62.105', '127.0.0.1', '104.196.180.37']
 
 
 # Application definition
