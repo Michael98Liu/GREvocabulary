820b0b207379c4534cc4e705ab1b3ca023e9461b
chennyhaohao <chennyhaohao@gmail.com>
2018-12-06 00:28:55 +0000

    updated models.py

--- GRE/models.py
+++ GRE/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+                User,
+                on_delete=models.CASCADE,
+                primary_key=True,
+            )
+    words = models.ManyToManyField("Word", through='Word_status')
+    
 
 class Category(models.Model):
     name = models.CharField(max_length = 80)
@@ -9,5 +19,20 @@ class Word(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE,
             null=True)
     
+class Word_status(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE )
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+    status_choices = (
+                ('DK', "Don't know"),
+                ('LN', 'Learning'),
+                ('K', 'Known'),
+            )
+    status = models.CharField(
+                max_length=2,
+                choices=status_choices,
+                default='DK',
+            )
+    correct_count = models.IntegerField(default=0)
+    wrong_count = models.IntegerField(default=0)
 
 # Create your models here.
