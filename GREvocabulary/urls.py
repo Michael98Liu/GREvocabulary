3d5130be1745c30bc8a3e302a28e887d5446a9fa
Michael98Liu <liufengyuan1998@hotmail.com>
2018-12-05 21:51:33 -0500

    auth setup

--- GREvocabulary/urls.py
+++ GREvocabulary/urls.py
@@ -23,4 +23,5 @@ from django.urls import include, path
 urlpatterns = [
     path('GRE/', include('GRE.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
