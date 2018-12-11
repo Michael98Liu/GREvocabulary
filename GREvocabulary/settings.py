799c0f1d54758bb0f59c545654928d75e341c72d
Michael98Liu <liufengyuan1998@hotmail.com>
2018-12-12 01:26:20 +0400

    add mem_cache

--- GREvocabulary/settings.py
+++ GREvocabulary/settings.py
@@ -120,6 +120,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
