532822b9f2351f79c85b9151de41410b7731d632
Yana Chala <ydc223@nyu.edu>
2018-12-11 01:51:06 +0000

    updating scripts

--- analytics/scripts/uploadScriptWordsPerUser.py
+++ analytics/scripts/uploadScriptWordsPerUser.py
@@ -4,10 +4,10 @@ import sys
 import glob
 import errno
 
-# mydb = mysql.connector.connect(user='user', password='pass',
-#                               host='localhost',
-#                               database='GRE')
-# mycursor = mydb.cursor()
+mydb = mysql.connector.connect(user='user', password='pass',
+                               host='localhost',
+                               database='GRE')
+mycursor = mydb.cursor()
 
 path = '/Users/yana/Documents/classes/Senior2018/LargeScaleWeb/GREWordApp/GREvocabulary/analytics/scripts/output/wordsPerUser/*'   
 files = glob.glob(path)   
@@ -19,19 +19,19 @@ for name in files: # 'file' is a builtin type, 'name' is a less-ambiguous variab
             	line = x.strip().split(" ")
             	user = line[0]
             	wordsLearnt = line[1]
-            	print(user, " : ", wordsLearnt)
-				# sql = "UPDATE Profile SET learnt_count=%s WHERE user = %s"
-				# val = (int(wordsLearnt), user)
-				# mycursor.execute(sql, val)
+            	#print(user, " : ", wordsLearnt)
+		sql = "UPDATE Profile SET learnt_count=%s WHERE user = %s"
+		val = (int(wordsLearnt), user)
+		mycursor.execute(sql, val)
 
     except IOError as exc:
         if exc.errno != errno.EISDIR: # Do not fail if a directory is found, just ignore it.
             raise # Propagate other kinds of IOError.
 
 
-# mydb.commit()
-# print(mycursor.rowcount, "record inserted.")
+mydb.commit()
+print(mycursor.rowcount, "record inserted.")
 
-# mycursor.close()
-# mydb.close()
+mycursor.close()
+mydb.close()
 
