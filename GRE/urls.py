259d56ab1c74b650938c442665c4a87333ec543f
Michael98Liu <liufengyuan1998@hotmail.com>
2018-12-11 07:33:32 +0400

    finish quiz

--- GRE/urls.py
+++ GRE/urls.py
@@ -6,7 +6,9 @@ from GRE.views import CategoryList, WordList
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('quiz/', login_required(CategoryList.as_view()), name="quiz"),
+    path('quiz/<str:word>/correct/<slug:category>', views.quizChooseCorrect),
+    path('quiz/<str:word>/wrong/<slug:category>', views.quizChooseWrong),
     path("quiz/<str:category>", login_required(WordList.as_view()), name="quizlist" ),
+    path('quiz/', login_required(CategoryList.as_view()), name="quiz"),
     path('learn/', views.learn, name='learn'),
 ]
--- GRE/views.py
+++ GRE/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 from django.http import HttpResponse
+from django.http import HttpRequest
+from django.shortcuts import redirect
 from django.views.generic import ListView
 from GRE.models import Category, Word
+
+import random
+import time
 
 class CategoryList(ListView):
     model = Category
@@ -11,9 +16,51 @@ class CategoryList(ListView):
 class WordList(ListView):
     model = Word
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        options = []
+        words_dict = Word.objects.filter(category__name=self.kwargs['category']).values('word')
+        meaning_dict = Word.objects.filter(category__name=self.kwargs['category']).values('meaning')
+        word_meaning = Word.objects.filter(category__name=self.kwargs['category'])
+        definitions = {}
+
+        # definitions: list of correct definition
+        for w in word_meaning:
+            definitions[w.word] = w.meaning
+
+        # options: list of options
+        for i in words_dict:
+            w = i['word']
+
+            opt = []
+            for j in meaning_dict:
+                if j['meaning'] not in opt:
+                    opt.append(j['meaning'])
+            random.shuffle(opt)
+            options.append([w, opt, definitions[w]] )
+
+        context["options"] = options
+        context['cat'] = self.kwargs['category']
+        return context
+
     def get_queryset(self):
-        print(self.kwargs['category'], type(self.kwargs['category']))
         return Word.objects.filter(category__name=self.kwargs['category'])
+
+def quizChooseCorrect(request, word, category):
+    # write to log
+    ts = int(time.time())
+    with open("./log/userChoices.log", "a") as f:
+        f.write("{} {} {} {}\n".format(request.user, word, "c", ts))
+    return redirect("/GRE/quiz/"+category)
+
+def quizChooseWrong(request, word, category):
+    # write to log
+    ts = int(time.time())
+    with open("./log/userChoices.log", "a") as f:
+        f.write("{} {} {} {}\n".format(request.user, word, "w", ts))
+    return redirect("/GRE/quiz/"+category)
 
 @login_required()
 def learn(request):
