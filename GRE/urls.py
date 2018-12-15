634250d90bba407cdba6e6f010633574c037a32c
Michael98Liu <liufengyuan1998@hotmail.com>
2018-12-15 06:12:08 +0400

    newquiz

--- GRE/urls.py
+++ GRE/urls.py
@@ -6,11 +6,13 @@ from GRE.views import CategoryList, WordList
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('quiz/<str:word>/correct/<slug:category>', views.quizChooseCorrect),
-    path('quiz/<str:word>/wrong/<slug:category>', views.quizChooseWrong),
-    path("quiz/<str:category>", login_required(WordList.as_view()), name="quizlist" ),
-    path('quiz/', login_required(CategoryList.as_view()), name="quiz"),
-    path('learn/', views.learn, name='learn'),
+    path('quiz/<str:word>/correct/', views.quizChooseCorrect),
+    path('quiz/<str:word>/wrong/', views.quizChooseWrong),
+    path("learn/<str:category>", login_required(WordList.as_view()), name="learnlist" ),
+    path('learn/', login_required(CategoryList.as_view()), name="learn"),
+    path('quiz/', views.learn, name='quiz'),
     path('log/', views.returnLog, name='log'),
-    path('signup/', views.signUp.as_view(), name='signup')
+    path('signup/', views.signUp.as_view(), name='signup'),
+    path('learn/<str:word>/learning/', views.learnLearning),
+    path('learn/<str:word>/know/', views.learnKnow),
 ]
--- GRE/views.py
+++ GRE/views.py
@@ -8,11 +8,12 @@ from django.views.generic import ListView
 from django.contrib.auth.forms import UserCreationForm
 from django.views import generic
 from django.urls import reverse_lazy
+from django.contrib.auth.models import User
 
 import random
 import time
 
-from GRE.models import Category, Word
+from GRE.models import Category, Word, Word_status, Profile
 
 class signUp(generic.CreateView):
     form_class = UserCreationForm
@@ -31,27 +32,30 @@ class WordList(ListView):
         # Add in a QuerySet of all the books
         options = []
         words_dict = Word.objects.filter(category__name=self.kwargs['category']).values('word')
-        meaning_dict = Word.objects.filter(category__name=self.kwargs['category']).values('meaning')
+        #meaning_dict = Word.objects.filter(category__name=self.kwargs['category']).values('meaning')
         word_meaning = Word.objects.filter(category__name=self.kwargs['category'])
         definitions = {}
-        diff = {}
+        #diff = {}
 
         # definitions: list of correct definition
         for w in word_meaning:
             definitions[w.word] = w.meaning
-            diff[w.word] = w.difficulty
+            #diff[w.word] = w.difficulty
 
-        # options: list of options
         for i in words_dict:
             w = i['word']
-
-            opt = []
-            for j in meaning_dict:
-                if j['meaning'] not in opt:
-                    opt.append(j['meaning'])
-            random.shuffle(opt)
-            options.append([w, opt, definitions[w], diff[w]] )
-
+            options.append([w, definitions[w]])
+        # options: list of options
+        # for i in words_dict:
+        #     w = i['word']
+        #
+        #     opt = []
+        #     for j in meaning_dict:
+        #         if j['meaning'] not in opt:
+        #             opt.append(j['meaning'])
+        #     random.shuffle(opt)
+        #     options.append([w, opt, definitions[w], diff[w]] )
+        #
         context["options"] = options
         context['cat'] = self.kwargs['category']
         return context
@@ -72,6 +76,22 @@ def quizChooseWrong(request, word, category):
     with open("./log/userChoices.log", "a") as f:
         f.write("{} {} {} {}\n".format(request.user, word, "w", ts))
     return redirect("/GRE/quiz/"+category)
+
+def learnKnow(request, word):
+    # write to log
+    print(request.user, word)
+    u = User.objects.get(username=request.user)
+    print(u)
+    p = Profile.objects.get(user=u)
+    print(p)
+    #w = Word_status.objects.get(word=word, user=Profile.objects.get(user=request.user))
+    #print(w)
+    return redirect("/GRE/learn/"+category)
+
+def learnLearning(request, word):
+    # write to log
+
+    return redirect("/GRE/learn/"+category)
 
 def returnLog(request):
     with open("./log/userChoices.log") as f:
